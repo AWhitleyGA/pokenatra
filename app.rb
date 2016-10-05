4f223181c69f2fc54278b56753d86aaf3b535f5e
Andrew Whitley <awhitley1233@gmail.com>
2016-10-05 11:44:54 -0400

    create route/new view added and functional

--- app.rb
+++ app.rb
@@ -11,6 +11,20 @@ get '/pokemon' do
   erb :"pokemons/index"
 end
 
+get '/pokemon/new' do
+  erb :"pokemons/new"
+end
+
+post '/pokemon' do
+  @pokemon = Pokemon.create(
+  name: params[:name],
+  cp: params[:cp],
+  poke_type: params[:poke_type],
+  img_url: params[:img_url]
+  )
+  redirect "/pokemon/#{@pokemon.id}"
+end
+
 get '/pokemon/:id' do
   @pokemon = Pokemon.find(params[:id])
   erb :"pokemons/show"
